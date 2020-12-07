47f92039073cf485ed9246ebc5bb578dbeed22e8
Mistesqr <mistesqr@gmail.com>
2020-12-07 20:57:39 +0200

    add method addTodoItem

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,29 +4,33 @@ import TodoList from '../todo-list/todo-list';
 import AppHeader from '../app-header/app-header';
 import SearchPanel from '../search-panel/search-panel';
 import ItemStatusFilter from '../item-status-filter/item-status-filter';
+import ItemAddForm from './../item-add-form/item-add-form';
 
 import "./app.scss";
 
 class App extends Component {
-  state = {
-    todoData: [
-        {
-            label: 'Drink Coffee',
-            important: false,
-            id: 1
-        },
-        {
-            label: 'Create React App',
-            important: true,
-            id: 2
-        },
-        {
-            label: 'Chill',
-            important: false,
-            id: 3
-        }
-    ]
-  };
+
+    maxId = 100;
+
+    state = {
+        todoData: [
+            {
+                label: 'Drink Coffee',
+                important: false,
+                id: 1
+            },
+            {
+                label: 'Create React App',
+                important: true,
+                id: 2
+            },
+            {
+                label: 'Chill',
+                important: false,
+                id: 3
+            }
+        ]
+    };
 
   deleteItem = (id)  => {
     this.setState(({ todoData }) => {
@@ -43,6 +47,23 @@ class App extends Component {
     });
   }
 
+    addItem = (text) => {
+        const newItem = {
+            label: text,
+            important: false,
+            id: this.maxId++
+        }
+
+        this.setState(({ todoData }) => {
+            return {
+                todoData: [
+                    ...todoData,
+                    newItem
+                ]
+            }
+        });
+    }
+
   render() { 
         return (
             <div className="todo-app">
@@ -53,6 +74,7 @@ class App extends Component {
             </div>
 
             <TodoList todos={this.state.todoData} onDeleted={ this.deleteItem }/>
+            <ItemAddForm onItemAdded={ this.addItem } />
             </div>
         );
     }
