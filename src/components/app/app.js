2b1f596f08cb053c75a3508e8029f9a6c6f2e69d
Mistesqr <mistesqr@gmail.com>
2020-12-07 20:11:29 +0200

    deleted todo-item method

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import TodoList from '../todo-list/todo-list';
 import AppHeader from '../app-header/app-header';
 import SearchPanel from '../search-panel/search-panel';
@@ -6,36 +7,55 @@ import ItemStatusFilter from '../item-status-filter/item-status-filter';
 
 import "./app.scss";
 
-const App = () => {
-  const todoData = [
-    {
-      label: 'Drink Coffee',
-      important: false,
-      id: 1
-    },
-    {
-      label: 'Create React App',
-      important: true,
-      id: 2
-    },
-    {
-      label: 'Chill',
-      important: false,
-      id: 3
+class App extends Component {
+  state = {
+    todoData: [
+        {
+            label: 'Drink Coffee',
+            important: false,
+            id: 1
+        },
+        {
+            label: 'Create React App',
+            important: true,
+            id: 2
+        },
+        {
+            label: 'Chill',
+            important: false,
+            id: 3
+        }
+    ]
+  };
+
+  deleteItem = (id)  => {
+    this.setState(({ todoData }) => {
+        const idx = todoData.findIndex((el) => el.id === id);
+
+        const before = todoData.slice(0, idx);
+        const after = todoData.slice(idx + 1);
+
+        const newData = [...before, ...after];
+
+        return {
+            todoData: newData
+        };
+    });
+  }
+
+  render() { 
+        return (
+            <div className="todo-app">
+            <AppHeader toDo={1} done={3} />
+            <div className="top-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
+
+            <TodoList todos={this.state.todoData} onDeleted={ this.deleteItem }/>
+            </div>
+        );
     }
-  ];
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
+}
 
 export default App;
