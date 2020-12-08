8486f03fc63fbe3d23372012b57983701dcc2718
Mistesqr <mistesqr@gmail.com>
2020-12-08 14:02:09 +0200

    upd toggle done method

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,38 +14,35 @@ class App extends Component {
 
     state = {
         todoData: [
-            {
-                label: 'Drink Coffee',
-                important: false,
-                id: 1
-            },
-            {
-                label: 'Create React App',
-                important: true,
-                id: 2
-            },
-            {
-                label: 'Chill',
-                important: false,
-                id: 3
-            }
+            this.createTodoItem('Drink Coffee'),
+            this.createTodoItem('Create React App'),
+            this.createTodoItem('Chill')
         ]
     };
 
-  deleteItem = (id)  => {
-    this.setState(({ todoData }) => {
-        const idx = todoData.findIndex((el) => el.id === id);
-
-        const before = todoData.slice(0, idx);
-        const after = todoData.slice(idx + 1);
-
-        const newData = [...before, ...after];
-
+    createTodoItem(label) {
         return {
-            todoData: newData
-        };
-    });
-  }
+            label,
+            important: false,
+            done: false,
+            id: this.maxId++
+        }
+    }
+
+    deleteItem = (id) => {
+        this.setState(({ todoData }) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+
+            const before = todoData.slice(0, idx);
+            const after = todoData.slice(idx + 1);
+
+            const newData = [...before, ...after];
+
+            return {
+                todoData: newData
+            };
+        });
+    }
 
     addItem = (text) => {
         const newItem = {
@@ -64,17 +61,47 @@ class App extends Component {
         });
     }
 
-  render() { 
+    onToggleImportant = (id) => {
+        console.log('toggle Important', id);
+    }
+
+    onToggleDone = (id) => {
+        this.setState(({ todoData }) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+
+            const oldItem = todoData[idx];
+            const newItem = {
+                ...oldItem,
+                done: !oldItem.done
+            }
+
+            const before = todoData.slice(0, idx);
+            const after = todoData.slice(idx + 1);
+
+            const newData = [...before, newItem, ...after];
+
+            return {
+                todoData: newData
+            };
+        });
+    }
+
+    render() {
         return (
             <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
+                <AppHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
 
-            <TodoList todos={this.state.todoData} onDeleted={ this.deleteItem }/>
-            <ItemAddForm onItemAdded={ this.addItem } />
+                <TodoList
+                    todos={this.state.todoData}
+                    onDeleted={this.deleteItem}
+                    onToggleImportant={this.onToggleImportant}
+                    onToggleDone={this.onToggleDone}
+                />
+                <ItemAddForm onItemAdded={this.addItem} />
             </div>
         );
     }
